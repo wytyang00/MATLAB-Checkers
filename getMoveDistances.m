070834d4a6f6df0ace250e89ba5f18a9b5229d46
Tyler Yang <0107wy@gmail.com>
2019-11-19 02:20:07 -0500

    Optimize for Speed

--- getMoveDistances.m
+++ getMoveDistances.m
@@ -6,45 +6,37 @@ function [moveDist] = getMoveDistances(empty, blackKing, black, redKing, red)
     % representing how far the piece can move in each of 4 directions
     % [Up-Left, Up-Right, Down-Left, Down-Right].
     
-%     moveDist = zeros(8, 8, 4);
-
-%     upLeft = zeros(8, 8);
-%     upLeft(2:8, 2:8) = (black(2:8, 2:8) | redKing(2:8, 2:8)) & empty(1:7, 1:7);
-%     upLeft(3:8, 3:8) = upLeft(3:8, 3:8) + 2 * (((black(3:8, 3:8) & red(2:7, 2:7)) | (redKing(3:8, 3:8) & black(2:7, 2:7))) & empty(1:6, 1:6));
-%     
-%     upRight = zeros(8, 8);
-%     upRight(2:8, 1:7) = (black(2:8, 1:7) | redKing(2:8, 1:7)) & empty(1:7, 2:8);
-%     upRight(3:8, 1:6) = upRight(3:8, 1:6) + 2 * (((black(3:8, 1:6) & red(2:7, 2:7)) | (redKing(3:8, 1:6) & black(2:7, 2:7))) & empty(1:6, 3:8));
-%     
-%     downLeft = zeros(8, 8);
-%     downLeft(1:7, 2:8) = (red(1:7, 2:8) | blackKing(1:7, 2:8)) & empty(2:8, 1:7);
-%     downLeft(1:6, 3:8) = downLeft(1:6, 3:8) + 2 * (((red(1:6, 3:8) & black(2:7, 2:7)) | (blackKing(1:6, 3:8) & red(2:7, 2:7))) & empty(3:8, 1:6));
-%     
-%     downRight = zeros(8, 8);
-%     downRight(1:7, 1:7) = (red(1:7, 1:7) | blackKing(1:7, 1:7)) & empty(2:8, 2:8);
-%     downRight(1:6, 1:6) = downRight(1:6, 1:6) + 2 * (((red(1:6, 1:6) & black(2:7, 2:7)) | (blackKing(1:6, 1:6) & red(2:7, 2:7))) & empty(3:8, 3:8));
-%     
-%     moveDist(:, :, 1) = upLeft;
-%     moveDist(:, :, 2) = upRight;
-%     moveDist(:, :, 3) = downLeft;
-%     moveDist(:, :, 4) = downRight;
-    
-    % These do the same task as shown above, but this way proved to be faster
-    
     moveDist = zeros(8, 8, 4);
     blackOrRedKing = (black | redKing);
     redOrBlackKing = (red | blackKing);
+    
+    % Pre-allocating shared indices
+    f  = 1:7;
+    fs = 1:6;
+    l  = 2:8;
+    ls = 3:8;
+    
+    middleBlack = black(2:7, 2:7);
+    middleRed   = red(2:7, 2:7);
 
-    moveDist(2:8, 2:8, 1) = blackOrRedKing(2:8, 2:8) & empty(1:7, 1:7);
-    moveDist(3:8, 3:8, 1) = moveDist(3:8, 3:8, 1) + 2 * (((black(3:8, 3:8) & red(2:7, 2:7)) | (redKing(3:8, 3:8) & black(2:7, 2:7))) & empty(1:6, 1:6));
+    % Upper-Left Move
+    moveDist(l, l, 1) = blackOrRedKing(l, l) & empty(f, f);
+    % Upper-Left Capture
+    moveDist(ls, ls, 1) = moveDist(ls, ls, 1) + 2 * (((black(ls, ls) & middleRed) | (redKing(ls, ls) & middleBlack)) & empty(fs, fs));
 
-    moveDist(2:8, 1:7, 2) = blackOrRedKing(2:8, 1:7) & empty(1:7, 2:8);
-    moveDist(3:8, 1:6, 2) = moveDist(3:8, 1:6, 2) + 2 * (((black(3:8, 1:6) & red(2:7, 2:7)) | (redKing(3:8, 1:6) & black(2:7, 2:7))) & empty(1:6, 3:8));
+    % Upper-Right Move
+    moveDist(l, f, 2) = blackOrRedKing(l, f) & empty(f, l);
+    % Upper-Right Capture
+    moveDist(ls, fs, 2) = moveDist(ls, fs, 2) + 2 * (((black(ls, fs) & middleRed) | (redKing(ls, fs) & middleBlack)) & empty(fs, ls));
 
-    moveDist(1:7, 2:8, 3) = redOrBlackKing(1:7, 2:8) & empty(2:8, 1:7);
-    moveDist(1:6, 3:8, 3) = moveDist(1:6, 3:8, 3) + 2 * (((red(1:6, 3:8) & black(2:7, 2:7)) | (blackKing(1:6, 3:8) & red(2:7, 2:7))) & empty(3:8, 1:6));
+    % Lower-Left Move
+    moveDist(f, l, 3) = redOrBlackKing(f, l) & empty(l, f);
+    % Lower-Left Capture
+    moveDist(fs, ls, 3) = moveDist(fs, ls, 3) + 2 * (((red(fs, ls) & middleBlack) | (blackKing(fs, ls) & middleRed)) & empty(ls, fs));
 
-    moveDist(1:7, 1:7, 4) = redOrBlackKing(1:7, 1:7) & empty(2:8, 2:8);
-    moveDist(1:6, 1:6, 4) = moveDist(1:6, 1:6, 4) + 2 * (((red(1:6, 1:6) & black(2:7, 2:7)) | (blackKing(1:6, 1:6) & red(2:7, 2:7))) & empty(3:8, 3:8));
+    % Lower-Right Move
+    moveDist(f, f, 4) = redOrBlackKing(f, f) & empty(l, l);
+    % Lower-Right Capture
+    moveDist(fs, fs, 4) = moveDist(fs, fs, 4) + 2 * (((red(fs, fs) & middleBlack) | (blackKing(fs, fs) & middleRed)) & empty(ls, ls));
     
 end
